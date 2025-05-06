2f8e49d775721896bd6ec5b08e29096cbb56fea1
RNA <133764521+RNA6@users.noreply.github.com>
2025-05-07 01:54:21 +0300

    Add files via upload

---
+++ Homeowner.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smart_home_system;
+
+
+public class Homeowner {
+    
+}
---
+++ Smart_Home_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package smart_home_system;
+
+
+public class Smart_Home_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
