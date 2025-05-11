c6c15ea61ad4612db1c0cf6e0dccd5a87b8caab8
RNA <133764521+RNA6@users.noreply.github.com>
2025-05-11 23:05:51 +0300

    Update SystemDatabase.java

--- SystemDatabase.java
+++ SystemDatabase.java
@@ -12,24 +12,23 @@ public class SystemDatabase {
     private static ArrayList<String> device_ids = new ArrayList();
     private static ArrayList<Device> devices = new ArrayList();
     private static ArrayList<LinkedDevice> all_links = new ArrayList();
+    private static ArrayList<FamilyMember> familyMembers = new ArrayList();
+    private static ArrayList<Device> malfunctioning_devices = new ArrayList();
     
     public static void add_home_user(String id){        
         home_user_ids.add(id);
-    }
-    
-    public static void add_device(){        
-        //Sarah code
     }
     
     public static void add_link(String oid, String did){
         LinkedDevice link = new LinkedDevice(oid, did);
         all_links.add(link);
     }
-    
-    public static void display_devices(){
-        //Rahaf code
+
+    public static ArrayList<Device> get_malfunctioning_devices() {
+        return malfunctioning_devices;
     }
-    
-    
-    
+
+    public static ArrayList<FamilyMember> getFamilyMembers() {
+        return familyMembers;
+    }    
 }
