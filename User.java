22c1e31f287e6c7ffed44bfebb91c5f27aa539a9
RNA <133764521+RNA6@users.noreply.github.com>
2025-05-11 22:42:06 +0300

    Update User.java

--- User.java
+++ User.java
@@ -3,15 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package smart_home_system;
-import java.util.ArrayList;
+import java.util.*;
 
-public class User {
+abstract public class User {
     private String username;
     private String user_id;
     private String password;
     private String email;
     private String date_of_birth;
-    private ArrayList<String>notification;
+    private boolean logged_in;
+    private ArrayList<String> notifications;
 
     public User(String username , String user_id , String password , String email , String date_of_birth){
         this.username = username;
@@ -19,66 +20,83 @@ public class User {
         this.password = password;
         this.email = email;
         this.date_of_birth = date_of_birth;
-        this.notification = new ArrayList<>();
+        this.logged_in = false;
+        this.notifications = new ArrayList<>();
     }
 
 
-    public String getUsername() {
+    public String get_username() {
         return username;
     }
 
 
-    public String getUser_id() {
+    public String get_user_id() {
         return user_id;
     }
 
 
-    public String getPassword() {
+    public String get_password() {
         return password;
     }
 
 
-     public String getEmail() {
+     public String get_email() {
         return email;
     }
 
 
-     public String getDate_of_birth() {
+     public String get_date_of_birth() {
         return date_of_birth;
     }
 
 
-    public void setUsername(String username) {
+    public void set_username(String username) {
         this.username = username;
     }
 
 
-    public void setUser_id(String user_id) {
+    public void set_user_id(String user_id) {
         this.user_id = user_id;
     }
 
 
-    public void setPassword(String password) {
+    public void set_password(String password) {
         this.password = password;
     }
 
 
-    public void setEmail(String email) {
+    public void set_email(String email) {
         this.email = email;
     }
 
 
-    public void setDate_of_birth(String date_of_birth) {
+    public void set_date_of_birth(String date_of_birth) {
         this.date_of_birth = date_of_birth;
     }
 
-
-    public void login(){
-        System.out.println("User logged in: " + username);
+    public boolean is_logged_in() {
+        return logged_in;
     }
 
+    public void login(){
+        Scanner input = new Scanner(System.in);
+        String username;
+        String password;
+        
+        System.out.print("Enter Username: ");
+        username = input.nextLine();
+        
+        System.out.print("Enter Password: ");
+        password = input.nextLine();
+        
+        if(username == this.username && password == this.password){
+            System.out.println("User logged in: " + username);
+            logged_in = true;
+        }
+    }
 
     public void logout(){
+        logged_in = false;
         System.out.println("User logged out: " + username);
     }
 
@@ -87,24 +105,25 @@ public class User {
         System.out.println("Attempting to switch users....");
     }
 
-
-    public void send_notification(){
-        System.out.println("Sending notification to user: " + username);
+    //System.out.println("Sending notification to user: " + username);
+    abstract public void send_notification(User user);
+    
+    public void recieve_notification(String notification){
+        notifications.add(notification);
     }
 
-
     public void display_function(){
-        System.out.println("Displaying user function....");
+        System.out.println(this.username + "Main Menu");
     }
 
 
     public void display_notification(){
         System.out.println("Notification for user " + username + ": ");
-        if(notification.isEmpty()){
+        if(notifications.isEmpty()){
             System.out.println("No notification.");
         }
         else{
-            for(String notificaion : notification){
+            for(String notification : notifications){
                 System.out.println("-" + notification);
             }
         }
